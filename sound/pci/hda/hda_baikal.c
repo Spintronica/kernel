dc711118ace810bc6808702e0a6010f51b3d857f
Vadim V. Vlasov <vadim.vlasov@elpitech.ru>
2026-02-24 20:11:54 +0300

    sound: hda-baikal: Add support for patch loader

--- sound/pci/hda/hda_baikal.c
+++ sound/pci/hda/hda_baikal.c
@@ -377,6 +377,7 @@ static int hda_baikal_create(struct snd_card *card,
 	INIT_WORK(&hda->irq_pending_work, azx_irq_pending_work);
 
 	chip->codec_probe_mask = 0x3; /* two codecs */
+	chip->jackpoll_interval = msecs_to_jiffies(100); /* 100ms */
 
 	chip->single_cmd = false;
 	chip->snoop = true;
@@ -539,6 +540,10 @@ static void hda_baikal_probe_work(struct work_struct *work)
 	struct platform_device *pdev = to_platform_device(hda->dev);
 	int max_slots;
 	int err;
+#ifdef CONFIG_SND_HDA_PATCH_LOADER
+	const void *patch_data;
+	int patch_len;
+#endif
 
 	pm_runtime_get_sync(hda->dev);
 	err = hda_baikal_first_init(chip, pdev);
@@ -565,6 +570,17 @@ static void hda_baikal_probe_work(struct work_struct *work)
 
 	if (err < 0)
 		goto out_free;
+
+#ifdef CONFIG_SND_HDA_PATCH_LOADER
+	patch_data = of_get_property(bus->dev->of_node, "patch-data", &patch_len);
+	if (patch_data) {
+		err = snd_hda_load_patch(&chip->bus, patch_len - 1, patch_data);
+		if (err < 0) {
+			dev_err(bus->dev, "Can't load patch - %d\n", err);
+			goto out_free;
+		}
+	}
+#endif
 
 	err = azx_codec_configure(chip);
 	if (err < 0)
